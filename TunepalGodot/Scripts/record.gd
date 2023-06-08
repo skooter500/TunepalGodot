96d2696626628a01633591a148f67d98aedb376f
Devin Daley <devindaley@dhcp-892b2737.ucd.ie>
2023-06-08 17:59:59 +0100

    Record now spits out mostoccuring frequency

--- TunepalGodot/Scripts/record.gd
+++ TunepalGodot/Scripts/record.gd
@@ -3,10 +3,12 @@ extends Control
 var record_bus_index: int
 var record_effect: AudioEffectRecord
 var recording: AudioStream
+var spectrum: AudioEffectSpectrumAnalyzerInstance
 
 func _ready():
 	record_bus_index = AudioServer.get_bus_index("Record")
 	record_effect = AudioServer.get_bus_effect(record_bus_index, 0)
+	spectrum = AudioServer.get_bus_effect_instance(record_bus_index, 1)
 	
 	
 func start_recording():
@@ -15,12 +17,14 @@ func start_recording():
 func stop_recording():
 	record_effect.set_recording_active(false)
 	recording = record_effect.get_recording()
-
-	var array = recording.data.to_float32_array()
-	print(array)
-	var result = FFT.fft(array)
-	#print("RESULTS SIZE: ", result.size())
-	#print(result)
+	var frequency = 0
+	var big = 0
+	for i in range(0,500):
+		if (spectrum.get_magnitude_for_frequency_range(i,i+1)[0] > big):
+			big = spectrum.get_magnitude_for_frequency_range(i,i+1)[0]
+			frequency = i
+	print(frequency, " Hz ", big)
+	
 
 func _on_record_pressed():
 	if record_effect.is_recording_active():
